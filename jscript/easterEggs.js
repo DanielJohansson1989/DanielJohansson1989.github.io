5d07cc597044f00120963683aaf49be0712f83fa
Daniel Johansson <dannesnake@gmail.com>
2024-03-07 18:44:23 +0100

    Update easter egg message removed comments

--- jscript/easterEggs.js
+++ jscript/easterEggs.js
@@ -9,22 +9,19 @@ function changeBackground() {
 
 document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('easterEggMessage');
-    let combination = ''; // Håll reda på den inskrivna kombinationen
+    let combination = '';
   
-    // Visa modal-popup när rätt kombination skrivs in
     document.addEventListener('keydown', function(event) {
-      combination += event.key; // Lägg till den senaste tangentyckningen
+      combination += event.key;
   
-      // Kolla om kombinationen matchar den önskade kombinationen, t.ex. "1337"
       if (combination === '1337') {
-        modal.style.display = 'block'; // Visa modal-popup
-        combination = ''; // Återställ kombinationen
+        modal.style.display = 'block';
+        combination = '';
       }
     });
   
-    // Stäng modal-popup när användaren klickar på "X"
     var closeBtn = document.getElementsByClassName('close')[0];
     closeBtn.addEventListener('click', function() {
-      modal.style.display = 'none'; // Dölj modal-popup
+      modal.style.display = 'none';
     });
   });
